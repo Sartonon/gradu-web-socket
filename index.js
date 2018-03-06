b9d9244c5b3857b471027563106286aab70150b2
Santeri Rusila <santtu_1993@hotmail.com>
2018-03-06 11:46:44 +0200

    Updates

--- index.js
+++ index.js
@@ -3,28 +3,39 @@ const app = express();
 const http = require('http').Server(app);
 const WebSocketServer = require('ws').Server;
 const wss = new WebSocketServer({
-        port: 8080
-    });
- 
+  port: 8080
+});
+
+var messages = [];
+
 app.use(express.static('public'));
- 
+
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
- 
+
+app.get('/messages', function(req, res) {
+  res.send(messages);
+});
+
 wss.broadcast = function broadcast(data) {
-    wss.clients.forEach(function each(client) {
-        client.send(data);
-    });
+  wss.clients.forEach(function each(client) {
+    client.send(data);
+  });
 };
- 
+
 wss.on('connection', function(ws) {
-    ws.on('message', function(msg) {
-        const data = JSON.parse(msg);
-        if (data.message) wss.broadcast(JSON.stringify({ name: data.name, message: data.message }));
-    });
+  ws.on('message', function(msg) {
+    const data = JSON.parse(msg);
+    if (data.message) {
+      messages = messages.concat(data.message);
+      wss.broadcast(
+        JSON.stringify({ name: data.name, message: data.message })
+      );
+    }
+  });
 });
- 
+
 http.listen(3001, function() {
-    console.log('listening on port 3000');
+  console.log('listening on port 3000');
 });
